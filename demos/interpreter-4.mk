06cd2958142392dc70a6f22e6032f601c3af32eb
mvr <matt@example.com>
2025-04-02 17:48:18 -0700

    docs/tests for hooks

---
+++ demos/interpreter-4.mk
@@ -0,0 +1,14 @@
+#!/usr/bin/env -S ./compose.mk mk.interpret
+# demos/interpreter-4.mk: 
+#   Demonstrating compose.mk as an alternate interpreter for make.
+#   This example shows automatic installation of pre/post hooks.
+#
+# USAGE: ./demos/interpreter-4.mk main_target
+#
+# Main docs: https://robot-wranglers.github.io/compose.mk/signals#pre-post-hooks
+
+main_target.pre:; echo hello pre-hook
+main_target:; echo hello main
+main_target.post:; echo hello post-hook
+
+__main__:; $(call log,${red} USAGE: ./demos/interpreter-4.mk main_target)
