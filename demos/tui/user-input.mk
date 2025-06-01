033a1ed2513623f80fe33ec88f16836d41179c64
mvr <matt@example.com>
2025-05-31 22:03:03 -0700

    replace snippet with demo

--- demos/tui/user-input.mk
+++ demos/tui/user-input.mk
@@ -1,7 +1,8 @@
 #!/usr/bin/env -S make -f
 # demos/tui/user-input.mk: 
 #   Demonstrates user-input with a file selector
-#   USAGE: ./demos/tui/user-input.mk
+# USAGE: ./demos/tui/user-input.mk
+
 include compose.mk 
 
 __main__:
